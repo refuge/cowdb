0d89a2067d59cc57e58735acab0cd7ba5aa19e98
benoitc <bchesneau@gmail.com>
2014-06-17 15:13:06 +0200

    CDB-8 remove etap and prepare eunit tests

--- Makefile
+++ Makefile
@@ -25,9 +25,8 @@ deps:
 doc: dev
 	$(REBAR) -C rebar_dev.config doc skip_deps=true
 
-cover: dev
-	COVER=1 prove t/*.t
-	@$(ERL) -detached -noshell -eval 'etap_report:create()' -s init stop
+test:
+	$(REBAR) -C rebar_dev.config eunit skip_deps=true
 
 clean:
 	@$(REBAR) clean
@@ -42,33 +41,6 @@ dialyzer: compile
 	@dialyzer -Wno_return -c ebin
 
 
-
-#
-# TESTS
-#
-CBT_ETAP_DIR=$(BASE_DIR)/t
-export CBT_ETAP_DIR
-
-
-ERL_FLAGS=-pa $(BASE_DIR)/deps/*/ebin -pa $(BASE_DIR)/ebin -pa $(CBT_ETAP_DIR)
-export ERL_FLAGS
-
-test: devbuild testbuild
-	prove $(CBT_ETAP_DIR)/*.t
-
-verbose-test: devbuild testbuild
-	echo $(ERL_FLAGS)
-	prove -v $(CBT_ETAP_DIR)/*.t
-
-testbuild: testclean
-	@echo "==> init test environement"
-	@$(ERLC) -v -o $(CBT_ETAP_DIR) $(CBT_ETAP_DIR)/etap.erl
-	@$(ERLC) -v -o $(CBT_ETAP_DIR) $(CBT_ETAP_DIR)/test_util.erl
-
-testclean:
-	@rm -rf $(CBT_ETAP_DIR)/*.beam
-	@rm -rf $(CBT_ETAP_DIR)/temp.*
-
 # development
 #
 devclean:
@@ -79,6 +51,3 @@ devbuild: devdeps
 
 devdeps:
 	$(REBAR) -C rebar_dev.config get-deps
-
-
-
---
+++ test/cowdb_tests.hrl
@@ -0,0 +1,41 @@
+% Licensed under the Apache License, Version 2.0 (the "License"); you may not
+% use this file except in compliance with the License. You may obtain a copy of
+% the License at
+%
+%   http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+% License for the specific language governing permissions and limitations under
+% the License.
+
+-include_lib("eunit/include/eunit.hrl").
+
+-define(BUILDDIR, filename:absname(
+        filename:join([
+                filename:dirname(code:which(?MODULE)),
+                ".."]))).
+
+-define(TEMPDIR,
+    filename:join([
+            ?BUILDDIR, "test", "temp"])).
+
+-define(tempfile,
+    fun() ->
+        {A, B, C} = erlang:now(),
+        N = node(),
+        FileName = lists:flatten(io_lib:format("~p-~p.~p.~p", [N, A, B, C])),
+        filename:join([?TEMPDIR, FileName])
+    end).
+
+
+
+
+-define(tempdb,
+    fun() ->
+            Nums = tuple_to_list(erlang:now()),
+            Prefix = "eunit-test-db",
+            Suffix = lists:concat([integer_to_list(Num) || Num <- Nums]),
+            list_to_binary(Prefix ++ "-" ++ Suffix)
+    end).
