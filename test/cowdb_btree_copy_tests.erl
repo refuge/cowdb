feb01e926d0ee932dea1765e8e22cef590f3f980
benoitc <bchesneau@gmail.com>
2014-08-17 19:35:49 +0200

    cowdb is now under MPL 2

--- test/cowdb_btree_copy_tests.erl
+++ test/cowdb_btree_copy_tests.erl
@@ -1,3 +1,9 @@
+%%-*- mode: erlang -*-
+%%
+%% This Source Code Form is subject to the terms of the Mozilla Public
+%% License, v. 2.0. If a copy of the MPL was not distributed with this
+%% file, You can obtain one at http://mozilla.org/MPL/2.0/.
+%%
 -module(cowdb_btree_copy_tests).
 
 -include_lib("include/cowdb.hrl").
